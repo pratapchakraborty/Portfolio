0e0913120be46c2b36aa68c35bd61e3a82812799
pratapchakraborty <132057489+pratapchakraborty@users.noreply.github.com>
2023-07-05 19:27:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 4,
+    centeredSlides: true,
+    spaceBetween: 30,
+    grabCursor: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
